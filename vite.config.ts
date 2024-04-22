8d46b2fa0c0a86d205b8dce94bf967dcd39e5980
Roman Trashutin <tttrashutin@gmail.com>
2024-04-22 11:29:26 +0300

    fix: change favicon

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }]
-  }
+  },
+  base: '/saes-generators.github.io/'
 })
